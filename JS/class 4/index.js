feb50cd19a3de37318344f525f4706a532da351c
Mr_duck <gustavorochaduck@gmail.com>
2024-03-02 13:12:33 +0100

    a

---
+++ JS/class 4/index.js
@@ -0,0 +1,9 @@
+// Aula 4
+window.alert('This part is of the class 04')
+window.alert('My First text in JS')
+window.confirm('You liked my JS?')
+window.prompt('Who are You?')
+// Aula 5
+window.alert('This part is of the class 05')
+var y = window.confirm('You Like JS?')
+alert('{var}')
