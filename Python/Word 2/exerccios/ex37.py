29b3d0218b09465e4ff286d457b87db420b558e0
Mr_duck <93455237+gustavorochaduck@users.noreply.github.com>
2024-09-17 21:14:21 +0200

    exercicio 37 feito
    - Tenho que dar uma olhada de novo e refazer esse programa
    
    # porque eu IDE boa é pesada

--- Python/Word 2/exerccios/ex37.py
+++ Python/Word 2/exerccios/ex37.py
@@ -1,1 +1,20 @@
-ppsdf
+import math
+num = int(input('type a numeber: '))
+print('-=-'*20)
+print('''
+[1] - BINARY
+[2] - OCTAL)
+[3] - HEXADECIMAL''')
+selection_fuction = int(input('Select some exchange base: '))
+print('-=-'*20)
+
+if selection_fuction == 1:
+    print(f'{num} exchanged for BINARY is equal as: {bin(num)}')
+elif selection_fuction == 2:
+    print(f'{num} exchanged for OCTAL is equal as: {octal(num)}')
+elif selection_fuction == 3:
+    print(f'{num} exchanged for HEXADECIMAL is equal as: {hex(num)}')
+else:
+    print('You typed something wrong!!!')
+print('-=-'*20)
+input('') 
